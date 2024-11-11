5da926b6419a791b276caaf0f87cb8c5dff09133
hadi14250 <hadihk2k@gmail.com>
2024-11-11 19:45:23 +0400

    added more test cases for tip 3

--- tip3-jsSpreadSyntaxAndMap/spreadSyntaxAndMap.js
+++ tip3-jsSpreadSyntaxAndMap/spreadSyntaxAndMap.js
@@ -2,5 +2,14 @@ function contamination(text, char){
     return [...text].map((c) => char).join("")
   }
 
-  console.log(contamination("abc", 'z'))
+  console.log(contamination("abc", 'z'));
+  console.log(contamination("", 'z'));
+  console.log(contamination("hello world", '*'));
+  console.log(contamination("12345", '0'));
+  console.log(contamination("Test", 'T'));
+  console.log(contamination("a", 'b'));
+  console.log(contamination("abcdefghijk", 'x'));
+  console.log(contamination("Contamination", 'c'));
+  console.log(contamination(" ", '-'));
+  console.log(contamination("UpperCaseTEST", 'L'));
   
