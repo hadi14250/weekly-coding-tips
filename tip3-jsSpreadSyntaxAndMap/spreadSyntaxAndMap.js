9e0f8b23b0809440ba37c4438108d40c9b52dc26
hadi14250 <hadihk2k@gmail.com>
2024-12-17 19:35:19 +0400

    added more testcases

--- tip3-jsSpreadSyntaxAndMap/spreadSyntaxAndMap.js
+++ tip3-jsSpreadSyntaxAndMap/spreadSyntaxAndMap.js
@@ -52,3 +52,4 @@ console.log(contamination("{}{}{}{}{}{}{}{}{}{}", ''));
 console.log(contamination("()()()()()()()()()()", ''));
 console.log(contamination("{([([([[{()}]])])])}", ''));
 console.log(contamination("/////////////////////////", ''));
+console.log(contamination("\\\\\\\\\\\\\\\\\\\\\\\\", '|'));
