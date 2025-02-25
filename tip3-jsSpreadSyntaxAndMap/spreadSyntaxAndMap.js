bd78ddf76b65b48c5962a454807158e52bb2efef
hadi14250 <hadihk2k@gmail.com>
2025-02-26 00:42:37 +0400

    scene1

--- tip3-jsSpreadSyntaxAndMap/spreadSyntaxAndMap.js
+++ tip3-jsSpreadSyntaxAndMap/spreadSyntaxAndMap.js
@@ -122,3 +122,4 @@ console.log(contamination("``````````````````", '≥'));
 console.log(contamination("¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡", '≥'));
 console.log(contamination("™™™™™™™™™™™™™™™™™™™", '≥'));
 console.log(contamination("££££££££££££££££££££", '≥'));
+console.log(contamination("¢¢¢¢¢¢¢¢¢¢¢¢¢¢¢¢¢¢¢¢", '≥'));
