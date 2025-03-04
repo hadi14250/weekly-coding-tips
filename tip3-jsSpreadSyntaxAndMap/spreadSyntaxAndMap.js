7dcbaab8b66674334c65065230f14e073b350f63
hadi14250 <hadihk2k@gmail.com>
2025-03-04 09:43:58 +0400

    added more test cases

--- tip3-jsSpreadSyntaxAndMap/spreadSyntaxAndMap.js
+++ tip3-jsSpreadSyntaxAndMap/spreadSyntaxAndMap.js
@@ -128,4 +128,6 @@ console.log(contamination("§§§§§§§§§§§§§§§§§§§§", '≥'));
 console.log(contamination("¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶", '≥'));
 console.log(contamination("•••••••••••••••••••", '≥'));
 console.log(contamination("ªªªªªªªªªªªªªªªªªªª", '≥'));
+console.log(contamination("ººººººººººººººººººº", '≥'));
+
 
