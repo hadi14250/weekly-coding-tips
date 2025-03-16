56fa25e3384b1b450f821a27dbd93874405d9965
hadi14250 <hadihk2k@gmail.com>
2025-03-16 15:00:13 +0400

    added more test cases

--- tip3-jsSpreadSyntaxAndMap/spreadSyntaxAndMap.js
+++ tip3-jsSpreadSyntaxAndMap/spreadSyntaxAndMap.js
@@ -140,3 +140,4 @@ console.log(contamination("˙©†®∂ƒ©˙¥®´®ƒ©†®´®", '≥'));
 console.log(contamination("•¨∆´˙∂©ß∆˙∆´˚∂∆´´∆", '≥'));
 console.log(contamination("ß˙ø“‘‘‘‘‘‘‘“““““““", '≥'));
 console.log(contamination("««««««««««««««««««", '≥'));
+console.log(contamination("∑´®", '≥'));
